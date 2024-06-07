687fc183c0526c92e8b03060344a5a39f6c8d82b
KevinPatel <Kpatel9306@conestogac.on.ca>
2024-06-06 23:57:38 -0400

    Frontend reformation and add or change env

--- frontend/src/utils/getCurrentData.js
+++
@@ -1,6 +0,0 @@
-export const getCurrentDate=()=>{
-    const date=new Date();
-    const month = date.getMonth();
-    const year = date.getFullYear();
-    const time = date.toLocaleTimeString();
-}
